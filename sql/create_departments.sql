f58bdad6919cb72f84a74c0a6f993fe5978185c8
olgapavlova <olgapavlova@gmail.com>
2024-08-03 14:34:11 +0300

    sql scripts

---
+++ sql/create_departments.sql
@@ -0,0 +1,19 @@
+USE spiski;
+
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE IF NOT EXISTS departments (
+	id VARCHAR(15) PRIMARY KEY,
+	name VARCHAR(100),
+	budget INT,
+	counter INT DEFAULT 0
+); 
+
+LOAD DATA INFILE '/var/lib/mysql-files/departments.csv'
+	INTO TABLE departments
+	FIELDS TERMINATED BY ','
+	OPTIONALLY ENCLOSED BY '"'
+	IGNORE 1 LINES
+	(id, name, budget)
+;
+
